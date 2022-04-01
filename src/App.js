9c80960e97f2a8d07b6602b60f44c89f8cced529
SaiBingi <saibingi1208@gmail.com>
2022-04-01 17:32:33 +0000

    Simple Todos Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==> https://saisimpletodos.ccbp.tech
+
+import SimpleTodos from './components/SimpleTodos'
+
+import './App.css'
+
+const App = () => <SimpleTodos />
+
+export default App
